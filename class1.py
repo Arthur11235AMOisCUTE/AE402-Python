6965a9b67e32c0d19cac4ac97ea657490cbd8fb6
Arthur is so CUTE <55903332+Arthur11235AMOisCUTE@users.noreply.github.com>
2021-02-01 10:56:24 +0800

    print,input

---
+++ class1.py
@@ -0,0 +1,26 @@
+import time
+
+print("I'm Eric")
+print('He is "cool"')
+
+name = input('Who are you? (Please enter your name) : ')
+print('Hi! {} ~ How are you today~/n/n'.format(name))
+
+
+
+r = input('Enter the radius about the circle : ')
+
+pi = (3.14159265358979)
+
+a = ((int(r)**2)*pi)
+
+l = (2*(int(r))*pi)
+
+print('This is the radius which you want to know about the circle : {}'.format(r))
+
+print('This is the perimeter which you want to know about the circle : {}'.format(l))
+
+print('This is the area which you want to know about the circle : {}'.format(a))
+
+
+
